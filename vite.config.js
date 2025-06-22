c5b9437588f8edce8bce1350f4c99f10b6462be0
Alphred TATONG <alphredtatong@gmail.com>
2025-06-22 20:10:43 +0100

    mise a jour

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { sass } from 'sass'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,
+        // Optionnel : ajouter des chemins globaux
+        additionalData: `@use "src/sass/default/variable" as *;`
+      }
+    }
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          libs: ['lodash', 'axios']
+        }
+      }
+    }
+  }
 })
